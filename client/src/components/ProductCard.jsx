3f930863af15cbfe91d81cdf7a5b11b9e36f0179
Michael Kane <kanemike7@gmail.com>
2020-08-08 16:07:35 -0500

    Refactor axios requests to be done in one function

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-const axios = require('axios');
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -8,34 +7,50 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import CardMedia from '@material-ui/core/CardMedia';
 import { Grid } from '@material-ui/core';
+import StarRating from './StarRating.jsx';
+const axios = require('axios');
 
 class ProductCard extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       currentId: this.props.id,
+      url: 'http://52.26.193.201:3000',
       thumbnail: null,
-
-    }
+      category: null,
+      name: null,
+      price: null,
+      rating: null,
+      isLoaded: false,
+    };
+    this.getRating = this.getRating.bind(this);
   }
 
-
   componentDidMount() {
-    // get request to get info
-    let thumbnail;
-    let name;
-    let category;
-    let price;
-    let salePrice;
+    axios.all([this.getRating(), this.getNameAndCategory(), this.getThumbnailAndPrice()])
+      .then(axios.spread((rating, nameAndCategory, thumbnailAndPrice) => {
+        this.setState({
+          rating: rating.data.ratings,
+          name: nameAndCategory.data.name,
+          category: nameAndCategory.data.category,
+          thumbnail: thumbnailAndPrice.data.results[0].photos[0].thumbnail_url,
+          price: thumbnailAndPrice.data.results[0].original_price,
+          isLoaded: true,
+        });
+      }));
+  }
 
-    axios.get(`http://52.26.193.201:3000/products/${this.state.currentId}/styles`)
-      .then((response) => {
-        console.log('resp from get styles', response.data);
-      })
-      .catch((err) => {
-        console.log('Err getting styles', err);
-      });
+  getRating() {
+    return axios.get(`${this.state.url}/reviews/${this.state.currentId}/meta`);
+  }
+
+  getNameAndCategory() {
+    return axios.get(`${this.state.url}/products/${this.state.currentId}`);
+  }
+
+  getThumbnailAndPrice() {
+    return axios.get(`${this.state.url}/products/${this.state.currentId}/styles`);
   }
 
   // props.id... will be passed in and we will use that to get all other info.
@@ -52,6 +67,7 @@ class ProductCard extends React.Component {
     };
 
     const contentStyle = {
+      lineHeight: '1px',
       padding: '0',
       paddingLeft: '15px',
       height: '200px',
@@ -63,26 +79,25 @@ class ProductCard extends React.Component {
 
     return (
       <Grid item>
-        <Card style={cardStyle} raised>
-          <CardMedia
-            style={mediaStyle}
-            image="https://images.unsplash.com/photo-1560567546-4c6dbc16877b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-            title="something"
-          />
-          <CardContent style={contentStyle}>
-            {/*
-          1) Category
-          2) Name of product
-          3) Price
-          4) star review
-        */}
-            <Typography>CATEGORY</Typography>
-            <Typography>Name of Product</Typography>
-            <Typography>Price</Typography>
-            <Typography>Star reviews</Typography>
-          </CardContent>
-
-        </Card>
+        {
+          this.state.isLoaded
+            ? (
+              <Card style={cardStyle} raised>
+                <CardMedia
+                  style={mediaStyle}
+                  image={this.state.thumbnail || 'https://us.123rf.com/450wm/pavelstasevich/pavelstasevich1811/pavelstasevich181101029/112815932-stock-vector-no-image-available-icon-flat-vector-illustration.jpg?ver=6'}
+                  title="something"
+                />
+                <CardContent style={contentStyle}>
+                  <Typography>{this.state.category.toUpperCase()}</Typography>
+                  <Typography>{this.state.name}</Typography>
+                  <Typography>${this.state.price}</Typography>
+                  <StarRating rating={this.state.rating} />
+                </CardContent>
+              </Card>
+            )
+            : <p>Waiting for card data..</p>
+        }
       </Grid>
     )
   }
