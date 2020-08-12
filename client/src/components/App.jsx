2746b39d3fbc696f4202fc8af2a7801954db0230
Michael Kane <kanemike7@gmail.com>
2020-08-11 22:06:51 -0500

    Add variables and functions to keep track of first and last card elements

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -14,10 +14,19 @@ class App extends React.Component {
     this.state = {
       currentProduct: 1,
       relatedProductIds: null,
+      firstCard: null,
+      lastCard: null,
+      // nodes: [],
     };
+
+    this.containerRef = React.createRef();
+
+    this.nodes = [];
+    this.count = 0;
 
     this.handleClick = this.handleClick.bind(this);
     this.updateRelatedItems = this.updateRelatedItems.bind(this);
+    this.retrieveNodes = this.retrieveNodes.bind(this);
   }
 
   componentDidMount() {
@@ -29,6 +38,11 @@ class App extends React.Component {
       .then((response) => {
         this.setState({
           relatedProductIds: response.data,
+          firstCard: response.data[0],
+          lastCard: response.data[response.data.length - 1],
+        }, () => {
+          this.count = 0;
+          this.nodes = [];
         });
       })
       .catch((err) => {
@@ -45,16 +59,41 @@ class App extends React.Component {
     });
   }
 
+  isOverflown() {
+    console.log('this.containerRef.current.clientWidth', this.containerRef.current.clientWidth);
+    //el.scrollWidth > el.clientWidth ?
+    console.log('this.containerRef.current.scrollWidth', this.containerRef.current.scrollWidth);
+    console.log(`first card: ${this.state.firstCard}, last card ${this.state.lastCard}`);
+
+    // this.nodes.forEach((node) => console.log('node.id'));
+    console.log('this.state.nodes.length', this.nodes);
+  }
+
+  retrieveNodes(node) {
+    // let joined = this.state.nodes.concat(node);
+    this.count++;
+    console.log(`adding node ${node}`);
+    this.nodes.push(node);
+
+    if (this.state.relatedProductIds.length === this.count) {
+      this.isOverflown();
+    }
+  }
+
   render() {
     const containerStyle = {
       padding: '20px',
       border: '1px solid black',
       flexWrap: 'nowrap',
-      overflow: 'hidden',
+      overflow: 'auto',
     };
 
     const { currentProduct } = this.state;
     const { relatedProductIds } = this.state;
+
+    // if (relatedProductIds) {
+    //   this.isOverflown();
+    // }
 
     return (
       <div>
@@ -64,16 +103,16 @@ class App extends React.Component {
         <div>
           <h3 style={{ textAlign: 'center' }}>RELATED PRODUCTS</h3>
           <Grid container direction="row">
-            <Grid item sm={3} />
-            <Grid item container direction="row" spacing={3} sm={6} style={containerStyle}>
+            <Grid item sm={2} />
+            <Grid ref={this.containerRef} item container direction="row" spacing={3} sm={8} style={containerStyle}>
               {
                 relatedProductIds
                   ? relatedProductIds.map((id, key) =>
-                    <ProductCard id={id} key={key} handleClick={this.handleClick} />)
+                    <ProductCard id={id} key={key} productView={this.state.currentProduct} handleClick={this.handleClick} getNode={this.retrieveNodes} />)
                   : <Typography>Waiting for data...</Typography>
               }
             </Grid>
-            <Grid item sm={3} />
+            <Grid item sm={2} />
           </Grid>
         </div>
       </div>
