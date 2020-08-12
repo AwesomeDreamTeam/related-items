f5ec8d184099443b1f2a132d1062c3ef64400f1c
Michael Kane <kanemike7@gmail.com>
2020-08-12 14:36:28 -0500

    Add arrows with fade styling

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,6 +5,8 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import { Grid } from '@material-ui/core';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 const axios = require('axios');
 
 class App extends React.Component {
@@ -16,6 +18,12 @@ class App extends React.Component {
       relatedProductIds: null,
       firstCard: null,
       lastCard: null,
+      overflowRight: false,
+      overflowLeft: false,
+      lastCardEdge: null,
+      firstCardEdge: null,
+      containerRightEdge: null,
+      containerLeftEdge: null,
       // nodes: [],
     };
 
@@ -60,13 +68,23 @@ class App extends React.Component {
   }
 
   isOverflown() {
-    console.log('this.containerRef.current.clientWidth', this.containerRef.current.clientWidth);
-    //el.scrollWidth > el.clientWidth ?
-    console.log('this.containerRef.current.scrollWidth', this.containerRef.current.scrollWidth);
+    let last = this.nodes[this.nodes.length - 1];
+    let lastRect = last.getBoundingClientRect();
+    let contRect = this.containerRef.current.getBoundingClientRect();
+
+    this.setState({
+      lastCardEdge: lastRect.right,
+      containerRightEdge: this.containerRef.current.clientWidth,
+      containerLeftEdge: contRect.left,
+    });
+
+
+    // console.log('this.containerRef.current.clientWidth', this.containerRef.current.clientWidth);
+    // //el.scrollWidth > el.clientWidth ?
+    // console.log('this.containerRef.current.scrollWidth', this.containerRef.current.scrollWidth);
     console.log(`first card: ${this.state.firstCard}, last card ${this.state.lastCard}`);
 
     // this.nodes.forEach((node) => console.log('node.id'));
-    console.log('this.state.nodes.length', this.nodes);
   }
 
   retrieveNodes(node) {
@@ -83,17 +101,49 @@ class App extends React.Component {
   render() {
     const containerStyle = {
       padding: '20px',
-      border: '1px solid black',
+      // border: '1px solid black',
       flexWrap: 'nowrap',
       overflow: 'auto',
+    };
+
+    const leftGridStyle = {
+      // border: '1px solid red',
+      textAlign: 'right',
+      position: 'relative',
+      background: 'rgba(255, 255, 255, 1)',
+      zIndex: '1',
+    };
+
+    const leftArrowStyle = {
+      // border: '1px solid red',
+      position: 'absolute',
+      height: '100%',
+      width: '70px',
+      background: 'linear-gradient(90deg, rgba(255,255,255,1), rgba(255,255,255,1), rgba(255,255,255,1), rgba(255,255,255,.5), rgba(255,255,255,0))',
+    };
+
+    const rightGridStyle = {
+      backgroundColor: 'rgba(255, 255,255,0)',
+      // border: '1px solid red',
+      textAlign: 'left',
+      position: 'relative',
+      // background: 'rgba(255, 255, 255, 1)',
+      zIndex: '1',
+    };
+
+    const rightArrowStyle = {
+      marginLeft: '-52',
+      // border: '1px solid red',
+      position: 'absolute',
+      height: '100%',
+      width: '70px',
+      background: 'linear-gradient(90deg, rgba(255,255,255,0), rgba(255,255,255,.5), rgba(255,255,255,1), rgba(255,255,255,1), rgba(255,255,255,1))',
     };
 
     const { currentProduct } = this.state;
     const { relatedProductIds } = this.state;
 
-    // if (relatedProductIds) {
-    //   this.isOverflown();
-    // }
+    console.log('last card edge', this.state.lastCardEdge);
 
     return (
       <div>
@@ -103,8 +153,11 @@ class App extends React.Component {
         <div>
           <h3 style={{ textAlign: 'center' }}>RELATED PRODUCTS</h3>
           <Grid container direction="row">
-            <Grid item sm={2} />
-            <Grid ref={this.containerRef} item container direction="row" spacing={3} sm={8} style={containerStyle}>
+            <Grid item sm={2} lg={3} style={leftGridStyle}>
+              <ArrowBackIosIcon fontSize="small" style={leftArrowStyle} />
+            </Grid>
+            <Grid ref={this.containerRef} item container direction="row" spacing={4} sm={8} lg={6} style={containerStyle}>
+
               {
                 relatedProductIds
                   ? relatedProductIds.map((id, key) =>
@@ -112,7 +165,9 @@ class App extends React.Component {
                   : <Typography>Waiting for data...</Typography>
               }
             </Grid>
-            <Grid item sm={2} />
+            <Grid item sm={2} lg={3} style={rightGridStyle}>
+              <ArrowForwardIosIcon fontSize="small" style={rightArrowStyle} />
+            </Grid>
           </Grid>
         </div>
       </div>
