cf2059c09a67676296b7222fbc805503ead1977b
Michael Kane <kanemike7@gmail.com>
2020-08-19 11:17:04 -0500

    Add theme to application

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
-import { ThemeProvider } from '@material-ui/core/styles';
-import theme from './components/theme.jsx';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import App from './components/App.jsx';
+import theme from './components/theme.jsx';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
