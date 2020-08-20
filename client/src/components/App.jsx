23abfdf344a2e29b9e53048639b087d4dbe28bb2
Michael Kane <kanemike7@gmail.com>
2020-08-20 00:29:32 -0500

    Add handler functions for adding and removing items from outfit, add placeholder cards for loading, add styling for section headers, and misc styling

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { Grid } from '@material-ui/core';
+import CardMedia from '@material-ui/core/CardMedia';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 import AddItemCard from './AddItemCard.jsx';
 import ProductCard from './ProductCard.jsx';
+import OutfitCard from './OutfitCard.jsx';
 
 const axios = require('axios');
 
@@ -19,6 +23,7 @@ class App extends React.Component {
       position: 0,
       endOfScroll: false,
       outfitList: [],
+      clickedItem: false,
     };
 
     this.containerRef = React.createRef();
@@ -28,6 +33,7 @@ class App extends React.Component {
     this.shiftRight = this.shiftRight.bind(this);
     this.updatePosition = this.updatePosition.bind(this);
     this.handleClickAddItem = this.handleClickAddItem.bind(this);
+    this.removeItemFromOutfit = this.removeItemFromOutfit.bind(this);
   }
 
   componentDidMount() {
@@ -54,6 +60,7 @@ class App extends React.Component {
           relatedProductIds: response.data,
           position: 0,
           endOfScroll: false,
+          clickedItem: false,
         });
       })
       .catch((err) => {
@@ -114,16 +121,42 @@ class App extends React.Component {
   }
 
   handleClickAddItem(e) {
-    console.log(`clicked ${this.state.currentProductName}`);
+    const clickedId = this.state.currentProduct;
+    const list = this.state.outfitList;
+    const isItemInList = list.includes(clickedId);
+    if (!this.state.clickedItem && !isItemInList) {
+      const joined = this.state.outfitList.concat(clickedId);
+      this.setState({
+        outfitList: joined,
+        clickedItem: true,
+      });
+    }
+  }
+
+  removeItemFromOutfit(itemId) {
+    let tempList = this.state.outfitList;
+    let index = tempList.indexOf(itemId);
+    tempList.splice(index, 1);
+    if (index !== -1) {
+      this.setState({
+        outfitList: tempList,
+        clickedItem: false,
+      });
+    }
   }
 
   render() {
     const containerStyle = {
-      // padding: '20px',
+      marginTop: '10px',
       flexWrap: 'nowrap',
       overflow: 'hidden',
     };
-
+    const outfitContainerStyle = {
+      marginTop: '10px',
+      flexWrap: 'nowrap',
+      overflow: 'scroll',
+      // boxShadow: '0 0 1px #FFF',
+    };
     const leftGridStyle = {
       textAlign: 'right',
       position: 'relative',
@@ -156,9 +189,23 @@ class App extends React.Component {
     };
 
     const headerStyle = {
-      textAlign: 'center',
-      fontSize: '24px',
+      marginLeft: '15px',
+      fontSize: '22px',
       fontWeight: 'lighter',
+    };
+    const cardStyle = {
+      height: '500px',
+      width: '315px',
+    };
+
+    const mediaStyle = {
+      width: 'auto',
+      height: '300px',
+    };
+
+    const contentStyle = {
+      height: '200px',
+      backgroundColor: 'transparent',
     };
 
     const { currentProduct } = this.state;
@@ -170,7 +217,14 @@ class App extends React.Component {
         <span>current product:</span>
         <span>{currentProduct}</span>
         <div>
-          <h3 style={headerStyle}>RELATED PRODUCTS</h3>
+          <Grid container direction="row">
+            <Grid item sm={2} lg={3} />
+            <Grid item container direction="row" spacing={4} sm={8} lg={6}>
+              <h3 style={headerStyle}>RELATED PRODUCTS</h3>
+            </Grid>
+            <Grid item sm={2} lg={3} />
+          </Grid>
+
           <Grid container direction="row">
             <Grid item sm={2} lg={3} style={leftGridStyle}>
               {
@@ -184,7 +238,15 @@ class App extends React.Component {
                 relatedProductIds
                   // eslint-disable-next-line max-len
                   ? relatedProductIds.map((id, key) => <ProductCard id={id} key={key} productView={this.state.currentProduct} handleClick={this.handleClick} />)
-                  : <Typography>Waiting for data...</Typography>
+                  : (
+                    <Card style={cardStyle}>
+                      <CardMedia
+                        style={mediaStyle}
+                        image="https://media0.giphy.com/media/3oEjI6SIIHBdRxXI40/200.gif"
+                      />
+                      <CardContent style={contentStyle} />
+                    </Card>
+                  )
               }
             </Grid>
             <Grid item sm={2} lg={3} style={rightGridStyle}>
@@ -196,11 +258,19 @@ class App extends React.Component {
             </Grid>
           </Grid>
         </div>
-        <div style={{ paddingTop: '30px', paddingBottom: '25px' }}>
-          <h3 style={headerStyle}>MY OUTFIT</h3>
+        <div style={{ paddingTop: '50px', paddingBottom: '25px' }}>
+          <Grid container direction="row">
+            <Grid item sm={2} lg={3} />
+            <Grid item container direction="row" spacing={4} sm={8} lg={6}>
+              <h3 style={headerStyle}>MY OUTFIT</h3>
+            </Grid>
+            <Grid item sm={2} lg={3} />
+          </Grid>
+
+
           <Grid container diretion="row">
             <Grid item sm={2} lg={3} />
-            <Grid item container direction="row" spacing={4} sm={8} lg={6} style={containerStyle}>
+            <Grid item container direction="row" spacing={4} sm={8} lg={6} style={outfitContainerStyle}>
               {
                 this.state.currentProductName
                   ? (
@@ -212,8 +282,10 @@ class App extends React.Component {
                   : null
               }
               {
-                this.state.outfitList
-                  ? <h2>Items in outfitList</h2>
+                this.state.outfitList.length
+                  ? this.state.outfitList.map((id) => {
+                    return <OutfitCard itemId={id} key={id} handleRemoveItem={this.removeItemFromOutfit} />
+                  })
                   : null
               }
             </Grid>
